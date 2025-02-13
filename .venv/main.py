2a4bb93897546c534d072c76310af173351b9792
avneetvirdi <avneetvirdi26@gmail.com>
2025-02-12 22:32:33 -0500

    gitignore fixed

---
+++ .venv/main.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+
+# Import data and name the columns as followed
+cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+df = pd.read_csv('./Data/magic04.data', names=cols);
+# print(df.head())
+
+# Check unique occurences of class column
+# print(df["class"].unique())
+
+# Since computers cant understand letters that much
+# convert g and h to 1 and 0
+# astype(int) converts the selected column to integer
+df["class"] = (df["class"] == "g").astype(int)
+#print(df.head());
+
+#=========================================================================
+# Plot all the features into a histogram
+# histogram represents the distribution of a value in the dataset
+
+# for label in cols[: -1]:
+#     plt.hist(df[df['class']==1][label], color='blue', label='gamma', alpha=0.7, density=True)
+#     plt.hist(df[df['class']==0][label], color='red', label='hydron', alpha=0.7 , density=True)
+#     plt.title(label)
+#     plt.ylabel('Probability')
+#     plt.xlabel(label)
+#     plt.legend()
+#     plt.show()
+
+# =======================================================================
+# Make Train, valid and test datasets
+# split 60% to training, 60-80% to valid, 80-100% to test
+train, valid, test = np.split(df.sample(frac=1), [int(0.6 * len(df)), int(0.8 * len(df))])
+
+# some labels have a higher value than other
+# like one is in 100s and other is in 0.0s
+# so this can show imbalance and innacuracy in data because
+# model will give more importance to the higher value
+# thats why we are scaling the values
+def scale_dataset(dataframe, oversample=False):
+    X = dataframe[dataframe.columns[:-1]].values
+    y = dataframe[dataframe.columns[-1]].values
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # There are more values of gamma than hydron so we want to increase the values of hydron
+    if oversample:
+        ros = RandomOverSampler()
+        X, y = ros.fit_resample(X, y)
+
+    data = np.hstack((X, np.reshape(y, (-1, 1))))
+
+    return data, X, y
+
+train, X_train, y_train = scale_dataset(train, oversample=True)
+valid, X_valid, y_valid = scale_dataset(valid, oversample=False)
+test, X_test, y_test = scale_dataset(test, oversample=False)
+
+# ========================================================================
+# K Nearest Neighbours
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+
+knn_model = KNeighborsClassifier(n_neighbors=3 )
+knn_model.fit(X_train, y_train)
+
+y_pred = knn_model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+# ========================================================================
+# Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+nb_model = GaussianNB()
+nb_model = nb_model.fit(X_train, y_train)
+
+y_pred = nb_model.predict(X_test)
+print(classification_report(y_test, y_pred))
