a56ea4b94726eeb67143a5d21e3e469cddb0c25d
avneetvirdi <avneetvirdi26@gmail.com>
2025-02-13 19:37:43 -0500

    Neural network for classification complete

--- .venv/main.py
+++ .venv/main.py
@@ -79,4 +79,80 @@ nb_model = GaussianNB()
 nb_model = nb_model.fit(X_train, y_train)
 
 y_pred = nb_model.predict(X_test)
-print(classification_report(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
+
+
+# ========================================================================
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+
+lg_model = LogisticRegression()
+lg_model = lg_model.fit(X_train, y_train)
+y_pred = lg_model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+# =======================================================================
+# Support Vector Mechanism
+from sklearn.svm import SVC
+svc_model = SVC()
+svc_model = svc_model.fit(X_train, y_train)
+y_pred = svc_model.predict(X_test)
+#print(classification_report(y_test, y_pred))
+
+# =======================================================================
+# Neural Network for Classification
+import tensorflow as tf
+
+def plot_history(history):
+  fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+  ax1.plot(history.history['loss'], label='loss')
+  ax1.plot(history.history['val_loss'], label='val_loss')
+  ax1.set_xlabel('Epoch')
+  ax1.set_ylabel('Binary crossentropy')
+  ax1.grid(True)
+
+  ax2.plot(history.history['accuracy'], label='accuracy')
+  ax2.plot(history.history['val_accuracy'], label='val_accuracy')
+  ax2.set_xlabel('Epoch')
+  ax2.set_ylabel('Accuracy')
+  ax2.grid(True)
+
+  plt.show()
+
+# make layers of neural network
+# keep last layer as output layer and sigmoid for classification 0 or 1
+# nn_model = tf.keras.models.Sequential([
+#     tf.keras.layers.Dense(32, activation='relu', input_shape=(10, )),
+#     tf.keras.layers.Dense(32, activation='relu'),
+#     tf.keras.layers.Dense(1, activation='sigmoid')
+# ])
+
+def train_model(X_train, y_train, num_nodes, dropout_prob, lr, epochs, batch_size):
+    nn_model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(num_nodes, activation='relu', input_shape=(10, )),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(num_nodes, activation='relu'),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    nn_model.compile(optimizer=tf.keras.optimizers.Adam(lr), loss='binary_crossentropy', metrics=['accuracy'])
+
+    history = nn_model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2, verbose=0)
+
+    return nn_model, history
+
+least_loss_value = float('inf')
+least_loss_model = None
+
+epochs = 100
+for num_nodes in [16, 32, 64]:
+    for dropout_prob in [0, 0.2]:
+        for lr in [0.01, 0.005, 0.001]:
+            for batch_size in [32, 64, 128]:
+                print(f"{num_nodes} nodes, dropout {dropout_prob}, lr {lr}, batch size {batch_size}")
+                model, history = train_model(X_train, y_train, num_nodes, dropout_prob, lr, epochs, batch_size)
+                plot_history(history)
+                valid_loss = model.evaluate(X_valid, y_valid)[0]
+                if valid_loss < least_loss_value:
+                    least_loss_value = valid_loss
+                    least_loss_model = model
